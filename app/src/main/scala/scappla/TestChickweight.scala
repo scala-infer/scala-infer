cae2c98a1e2f1e855fd8768d7bb6207bd60d7b7b
Frank van Lankvelt <frnk@a-eskwadraat.nl>
2018-12-30 21:52:15 +0100

    use adam, linear approximation

--- app/src/main/scala/scappla/TestChickweight.scala
+++ app/src/main/scala/scappla/TestChickweight.scala
@@ -3,10 +3,10 @@ package scappla
 import java.io.InputStreamReader
 
 import com.github.tototoshi.csv.CSVReader
-import scappla.Functions.{exp, log}
+import scappla.Functions.exp
 import scappla.distributions.Normal
 import scappla.guides.ReparamGuide
-import scappla.optimization.SGDMomentum
+import scappla.optimization.Adam
 
 object TestChickweight extends App {
 
@@ -15,20 +15,21 @@ object TestChickweight extends App {
   val reader = CSVReader.open(new InputStreamReader(getClass.getResourceAsStream("/chickweight.csv")))
   val raw = reader.allWithHeaders()
 
-  case class Record(weight: Double, time: Int, chick: Int, diet: Int)
+  case class Record(weight: Double, time: Double, diet: Int)
 
   val data = raw.map { row =>
-    Record(row("weight").toDouble, row("Time").toInt, row("Chick").toInt, row("Diet").toInt)
+    Record(row("weight").toDouble, row("Time").toDouble, row("Diet").toInt)
   }
 
-  val sgd = new SGDMomentum(mass = 200)
-  val aPost = ReparamGuide(Normal(sgd.param(40.0, 1.0), exp(sgd.param(0.0, 1.0))))
-  val atPost = ReparamGuide(Normal(sgd.param(0.0, 1.0), exp(sgd.param(0.0, 1.0))))
+
+  val sgd = new Adam()
+  val aPost = ReparamGuide(Normal(sgd.param(40.0), exp(sgd.param(0.0))))
+  val atPost = ReparamGuide(Normal(sgd.param(0.0), exp(sgd.param(0.0))))
 
   val bPost = Range(1, 5).map { i =>
     i -> (
-        ReparamGuide(Normal(sgd.param(7.0, 1.0), exp(sgd.param(0.0, 1.0)))),
-        ReparamGuide(Normal(sgd.param(0.0, 1.0), exp(sgd.param(0.0, 1.0))))
+        ReparamGuide(Normal(sgd.param(10.0), exp(sgd.param(1.0)))),
+        ReparamGuide(Normal(sgd.param(0.0), exp(sgd.param(0.0))))
     )
   }.toMap
 
@@ -37,7 +38,7 @@ object TestChickweight extends App {
     val a_t = exp(sample(Normal(0.0, 0.2), atPost))
     val b = bPost.map { case (i, (muGuide, sGuide)) =>
       i -> (
-          sample(Normal(7.0, 2.0), muGuide),
+          sample(Normal(10.0, 3.0), muGuide),
           exp(sample(Normal(0.0, 1.0), sGuide))
       )
     }
@@ -46,7 +47,7 @@ object TestChickweight extends App {
       entry: Record =>
         val (b_mu, b_s) = b(entry.diet)
         observe(Normal(
-          a + b_mu * (entry.time * entry.time).toDouble / 20.0, a_t + b_s * entry.time.toDouble
+          a + b_mu * entry.time, a_t + b_s * entry.time
         ), entry.weight: Real)
     }
 
@@ -54,7 +55,7 @@ object TestChickweight extends App {
   }
 
   // warm up
-  val N = 10000
+  val N = 1000
   val startTime = System.currentTimeMillis()
   println("a,a_t,b1m,b1s,b2m,b2s,b3m,b3s,b4m,b4s")
   Range(0, N).foreach { i =>
@@ -63,7 +64,7 @@ object TestChickweight extends App {
     println(s"${v_a.v}, ${v_a_t.v}, ${
       v_b.toSeq.sortBy(_._1).map(_._2).map {
         case (m, s) => s"${m.v}, ${s.v}"
-      }.mkString(",")
+      }.mkString(", ")
     }")
   }
   val endTime = System.currentTimeMillis()
