02737f4c23d6db248bcefa48bba379cc2eb021f0
Frank van Lankvelt <frnk@a-eskwadraat.nl>
2019-02-08 21:10:55 +0100

    Implement SymDiff

--- core/src/main/scala/scappla/tensor/NotContains.scala
+++ core/src/main/scala/scappla/tensor/NotContains.scala
@@ -2,19 +2,24 @@ package scappla.tensor
 
 import shapeless._
 
-/*
 trait NotContains[L, X]
 
 object NotContains {
 
-  def apply[L, X](implicit n: NotContains[L, X]) = n
+  def apply[L <: Shape, X <: Dim[_]](implicit n: NotContains[L, X]) = n
 
-  implicit def notContainsHListCase0[X]: NotContains[Scalar, X]
-  = new NotContains[Scalar, X] {}
+  implicit def scalarContainsNothing[X <: Dim[_]]: NotContains[Scalar, X] = {
+    new NotContains[Scalar, X] {}
+  }
 
-  implicit def notContainsHListCaseN[H, T <: Shape, X]
-  (implicit t: NotContains[T, X], n: H =:!= X): NotContains[H :: T, X]
-  = new NotContains[H :: T, X] {}
+  implicit def neqDimsDontContain[H <: Dim[_], X <: Dim[_]]
+  (implicit n: H =:!= X): NotContains[H, X] = {
+    new NotContains[H, X] {}
+  }
+
+  implicit def higherOrderShape[H <: Dim[_], T <: Shape, X <: Dim[_]]
+  (implicit t: NotContains[T, X], n: H =:!= X): NotContains[H :#: T, X] = {
+    new NotContains[H :#: T, X] {}
+  }
 
 }
-*/
