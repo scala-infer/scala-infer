7d3a9b2acd8b4ba366b7e2cbc34ecff774cf8b2a
Frank van Lankvelt <frnk@a-eskwadraat.nl>
2019-03-21 20:54:05 +0100

    implement Categorical on a 1-tensor

--- core/src/main/scala/scappla/distributions/Categorical.scala
+++ core/src/main/scala/scappla/distributions/Categorical.scala
@@ -1,6 +1,5 @@
 package scappla.distributions
 
-import scappla.Real._
 import scappla.Functions.{log, sum}
 import scappla.tensor.{DataOps, Dim, GreaterThan, Tensor}
 import scappla.{Expr, Score}
@@ -9,21 +8,20 @@ import scala.util.Random
 
 case class Categorical[S <: Dim[_], D: DataOps](p: Expr[Tensor[S, D]]) extends Distribution[Int] {
 
-  import scappla.Real._
   import scappla.tensor.TensorExpr._
 
   private val total = sum(p)
 
   override def sample(): Int = {
     val draw = Random.nextDouble() * total.v
-    val cs = cumsum(p, p.v.shape) - broadcast(draw.const, p.v.shape)
-    count(cs, GreaterThan(0f))
+    val cs = cumsum(p, p.v.shape)
+    count(cs, GreaterThan(draw.toFloat))
   }
 
-  override def observe(value: Int): Score = {
+  override def observe(index: Int): Score = {
     import scappla.InferField._
-    val vals = collect(p.v)
-    log(vals(value) / total)
+    val value = at(p.v, index)
+    log(value / total)
   }
 
 }
